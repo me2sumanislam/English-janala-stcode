712699c654515579e3b7a7d631d8111432c6c000
me2sumanislam <me2sumanislam.247@gmail.com>
2026-02-11 10:24:55 +0600

    add dynamic caard

--- script/index.js
+++ script/index.js
@@ -4,6 +4,33 @@ const loadLessons = () => {
         .then((json) => displayLesson(json.data));
 };
 
+const loadLevelWord = (id) => {
+    const url = `https://openapi.programming-hero.com/api/level/${id}`
+    // console.log(url);
+    fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayLevelWord(data.data))
+};
+
+const displayLevelWord = (words) =>{
+//  console.log(words)
+const wordContainer = document.getElementById("word-Container");
+wordContainer.innerHTML = "";
+
+words.forEach((word) => {
+    // console.log(word);
+    const card = document.createElement("div")
+    card.innerHTML = `
+    <p>Cat</p>
+    
+    `;
+    wordContainer.append(card);
+});
+ 
+};
+
+
+
 const displayLesson = (lessons) => {
     // 1.get the container & empty
     const levelContainer = document.getElementById("level-Container");
@@ -14,7 +41,7 @@ const displayLesson = (lessons) => {
         console.log(lesson);
         const btnDiv = document.createElement("div");
         btnDiv.innerHTML = `
-       <button class= " btn btn-outline btn-primary ">      
+       <button onclick ="loadLevelWord(${lesson.level_no})" class= " btn btn-outline btn-primary ">      
         <i class="fa-solid fa-book-open"></i>Lesson -${lesson.level_no}
         </button>
    
